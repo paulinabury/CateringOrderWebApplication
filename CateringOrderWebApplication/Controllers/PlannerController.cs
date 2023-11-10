08b9a18bc63bdf2a205f86482035142279e47d7d
Paulina.Bury <javadevil321@gmail.com>
2023-11-10 20:10:08 +0100

    Add tags to catering cards, initial impl of catering planner

---
+++ CateringOrderWebApplication/Controllers/PlannerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CateringOrderWebApplication.Controllers
+{
+    public class PlannerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
